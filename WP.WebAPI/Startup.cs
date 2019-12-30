99feddc7168950b76b566d4e8e7d8c3f4f24f70f
Travis Primm <tp2124@gmail.com>
2019-12-29 16:10:13 -1000

    Adding nuget for codegen (except that it failed...)

--- WP.WebAPI/Startup.cs
+++ WP.WebAPI/Startup.cs
@@ -27,7 +27,7 @@ namespace WP.WebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<WPContext>(opt => opt.UseInMemoryDatabase("WaterPleaseData"))
+            services.AddDbContext<WPContext>(opt => opt.UseInMemoryDatabase("WaterPleaseData"));
             services.AddControllers();
         }
 
