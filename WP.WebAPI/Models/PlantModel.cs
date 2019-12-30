978bae2dc7bb501f6b6c1a2b9a66ca6b60ef0931
Travis Primm <tp2124@gmail.com>
2019-12-29 15:43:13 -1000

    Adding the initial model and the DB Context.

---
+++ WP.WebAPI/Models/PlantModel.cs
@@ -0,0 +1,7 @@
+namespace WP.WebAPI {
+    public class Plant {
+        public long Id { get; set; }
+        public string FriendlyName { get; set; }
+        public string ScientificName { get; set; }
+    }
+}
---
+++ WP.WebAPI/Models/WPContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace WP.WebAPI.Models {
+    public class WPContext : DbContext {
+        public WPContext(DbContextOptions<WPContext> options)
+            : base(options) {
+        }
+        
+        public DbSet<PlantModel> PlantModels { get; set;}
+    }
+}
